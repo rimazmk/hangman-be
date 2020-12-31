4a6ec2f607433eb4c4cda7774c4f11a00dcfa9c3
Srihari Srinivasan <ssrihari@umich.edu>
2020-12-31 03:30:14 -0500

    Update Style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, abort, request
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, emit
 from flask_redis import FlaskRedis
 from flask_cors import CORS
 
@@ -10,115 +10,104 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = b''.join([
+    b'\x92\xc6\xd6',
+    b'\x1e;\x19_fl\xc0',
+    b'\xf31\xc1N\xc0kT',
+    b'\xb0s\x7f\xa1',
+    b'\x8f\x03',
+])
+
 app.config['REDIS_URL'] = "redis://:@localhost:6379/0"
 
 socketio = SocketIO(app, cors_allowed_origins='*')
-print(socketio)
 redis_client = FlaskRedis(app)
+
+print(socketio)
 
 count = 0
 
+
 @socketio.on('connect')
-def connect():
+def handle_connect():
     global count
     count += 1
     print(count, " connected")
 
 
 @socketio.on('disconnect')
-def disconnect():
+def handle_disconnect():
     global count
     count -= 1
     print(count, " connected")
 
-# @socketio.on('connect')
-# def hello():
-#     print("client connected")
-
-@socketio.on('message')
-def handle_message(message):
-    # print('received message: ', message)
-    emit('response', message)
 
 @socketio.on('newRound')
-def handle_newRound(info):
-    # print('received message: ', message)
-    category = info['category']
-    word = info['word']
-    roomID = info["roomID"]
-    gameState = redis_client.get(roomID)
-    gameState = json.loads(gameState)
-    gameState['word'] = word
-    gameState['category'] = category
-    guessedWord = ['#' if c.isalnum() else c for c in word]
-    gameState['guessedWord'] = ''.join(guessedWord)
-    redis_client.set(roomID, json.dumps(gameState))
-    emit('response', gameState, broadcast=True)
+def handle_new_round(info):
+    word, roomID = info['word'], info['roomID']
+    game_state = json.loads(redis_client.get(roomID))
+    game_state['word'] = word
+    game_state['category'] = info['category']
+    game_state['guessedWord'] = ''.join(
+        ['#' if c.isalnum() else c for c in word])
+    redis_client.set(roomID, json.dumps(game_state))
+    emit('response', game_state, broadcast=True)
+
 
 @socketio.on('guess')
 def handle_guess(msg):
-    gameState = msg['gameState']
-    cur = gameState['curGuess']
+    game_state = msg['gameState']
+    cur = game_state['curGuess']
 
-    print(gameState, cur)
-    print("received guess!")
-
-    if len(cur) == 1:
-        gameState['guessedLetters'].append(cur.lower())
+    if len(cur) == 1 and cur.isalpha():
+        game_state['guessedLetters'].append(cur.lower())
         match = 0
-        # Frontend prevents user from guessing a previously guessed letter
-        for i, (w, g) in enumerate(zip(gameState['word'], gameState['guessedWord'])):
+
+        for i, (w, g) in enumerate(zip(game_state['word'], game_state['guessedWord'])):
             if w.lower() == cur.lower() and g == '#':
-                gameState['guessedWord'] = gameState['guessedWord'][:i] + cur + gameState['guessedWord'][i + 1:]
+                game_state['guessedWord'] = game_state['guessedWord'][:i] + \
+                    cur + game_state['guessedWord'][i + 1:]
                 match += 1
 
         if match == 0:
-            gameState['numIncorrect'] += 1
+            game_state['numIncorrect'] += 1
 
     else:
-        gameState['guessedWords'].append(cur)
+        game_state['guessedWords'].append(cur)
 
-        if cur.lower() != gameState['word'].lower():
-            gameState['numIncorrect'] += 1
+        if cur.lower() != game_state['word'].lower():
+            game_state['numIncorrect'] += 1
 
     # TODO: Update wins variable here
-    if (cur.lower() == gameState['word'].lower()
-        or gameState['numIncorrect'] == gameState['lives']
-        or gameState['word'].lower() == gameState['guessedWord'].lower()):
-        if gameState["numIncorrect"] == gameState['lives']:
-            hangPos = gameState['players'].index(gameState['hanger'])
-            guessPos = hangPos + 1 if hangPos != (len(gameState['players']) - 1) else 0
-            gameState['guesser'] = gameState['players'][guessPos]
-        else:
-            gameState["hanger"] = msg['user']
-            hangPos = gameState['players'].index(gameState['hanger'])
-            guessPos = hangPos + 1 if hangPos != (len(gameState['players']) - 1) else 0
-            gameState['guesser'] = gameState['players'][guessPos]
+    if (cur.lower() == game_state['word'].lower() or
+        game_state['numIncorrect'] == game_state['lives'] or
+            game_state['word'].lower() == game_state['guessedWord'].lower()):
 
-        # gameState['word'] = ""
-        gameState['category'] = ""
-        gameState['guessedLetters'] = []
-        gameState['numIncorrect'] = 0
-        gameState['guessedWords'] = []
-        gameState['curGuess'] = ""
-        gameState['guessedWord'] = ""
+        if game_state["numIncorrect"] != game_state['lives']:
+            game_state["hanger"] = msg['user']
+
+        hang_pos = game_state['players'].index(game_state['hanger'])
+        guess_pos = hang_pos + \
+            1 if hang_pos != (len(game_state['players']) - 1) else 0
+        game_state['guesser'] = game_state['players'][guess_pos]
+
+        # TODO: Transition to newRound without relying on category
+        # game_state['word'] = ""
+        game_state['category'] = game_state['curGuess'] = game_state['guessedWord'] = ""
+        game_state['guessedLetters'], game_state['guessedWords'] = [], []
+        game_state['numIncorrect'] = 0
     else:
-        guessPos = gameState['players'].index(gameState['guesser'])
-        hangPos = gameState['players'].index(gameState['hanger'])
-        guessPos = (guessPos + 1) % len(gameState['players'])
-        if guessPos == hangPos:
-            guessPos = (guessPos + 1) % len(gameState['players'])
-        gameState['guesser'] = gameState['players'][guessPos]
+        guess_pos = game_state['players'].index(game_state['guesser'])
+        guess_pos = (guess_pos + 1) % len(game_state['players'])
+        hang_pos = game_state['players'].index(game_state['hanger'])
 
-    redis_client.set(msg['roomID'], json.dumps(gameState))
-    emit('guess', gameState, broadcast=True)
+        if guess_pos == hang_pos:
+            guess_pos = (guess_pos + 1) % len(game_state['players'])
+        game_state['guesser'] = game_state['players'][guess_pos]
 
-# {
-#    user: username,
-#    roomId: roomid,
-#    gameState: newGameState
-# }
+    redis_client.set(msg['roomID'], json.dumps(game_state))
+    emit('guess', game_state, broadcast=True)
+
 
 @socketio.on('join')
 def handle_join(credentials):
@@ -127,60 +116,61 @@ def handle_join(credentials):
     game_state = json.loads(redis_client.get(roomID))
 
     # TODO: handle disconnected player in else
-    if game_state['gameStart'] == False:
+    if not game_state['gameStart']:
         # TODO: account for existing username
         game_state['players'].append(username)
         redis_client.set(roomID, json.dumps(game_state))
-        emit('join', json.loads(redis_client.get(roomID)), broadcast=True)
+        emit('join', game_state, broadcast=True)
+
 
 @socketio.on('start')
 def handle_start(roomID):
     game_state = json.loads(redis_client.get(roomID))
     game_state['gameStart'] = True
     game_state['guesser'] = game_state["players"][1]
-    print(f"here are the players: {game_state['players']}")
+    # print(f"Players: {game_state['players']}")
     redis_client.set(roomID, json.dumps(game_state))
-    emit('start', json.loads(redis_client.get(roomID)), broadcast=True)
+    emit('start', game_state, broadcast=True)
 
 
-@app.route("/", methods=['GET','POST'])
-def show_index():
+@app.route("/")
+def get_state():
     roomID = request.args.get("roomID", default="", type=str)
     if roomID and redis_client.exists(roomID):
         return json.loads(redis_client.get(roomID))
     return abort(404)
 
+
 @socketio.on("create")
 def create_game(request):
-    # print("Creating room with params ", request)
-    roomID = ''.join(random.choices(string.ascii_uppercase +
-                                    string.digits, k=10))
-    while redis_client.exists(roomID):
+    while True:
         roomID = ''.join(random.choices(string.ascii_uppercase +
                                         string.digits, k=10))
+        if not redis_client.exists(roomID):
+            break
 
-    guessedWord = ['#' if c.isalnum() else c for c in request['word']]
+    guessed_word = ''.join(
+        ['#' if c.isalnum() else c for c in request['word']])
 
     # TODO: Get word and category input at game screen
-    defGameState = {'players': [request['username']],
-                    'hanger': request['username'],
-                    'category': request['category'],
-                    'word': request['word'],
-                    'guessedLetters': [],
-                    'numIncorrect': 0,
-                    'lives': int(request['lives']),
-                    'guessedWords': [],
-                    'guesser': None,
-                    'curGuess': None,
-                    'guessedWord': ''.join(guessedWord),
-                    'gameStart': False,
-                    }
+    def_game_state = {'players': [request['username']],
+                      'hanger': request['username'],
+                      'category': request['category'],
+                      'word': request['word'],
+                      'guessedLetters': [],
+                      'numIncorrect': 0,
+                      'lives': int(request['lives']),
+                      'guessedWords': [],
+                      'guesser': None,
+                      'curGuess': None,
+                      'guessedWord': guessed_word,
+                      'gameStart': False,
+                      }
 
-    redis_client.set(roomID, json.dumps(defGameState))
-    # print(roomID, redis_client.exists(roomID))
-
-    response = {'gameState': defGameState, 'roomID': roomID}
+    redis_client.set(roomID, json.dumps(def_game_state))
+    response = {'gameState': def_game_state, 'roomID': roomID}
     emit('link', response)
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from main import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug=True)
